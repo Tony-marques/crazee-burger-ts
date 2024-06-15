133229f7948e97db8a9c06cf7b2b3512c1b7c40f
tonym <tony.marques@live.fr>
2024-06-15 12:47:09 +0200

    💄 F03: LOGO > Styling Logo

--- src/components/common/Logo/Logo.tsx
+++ src/components/common/Logo/Logo.tsx
@@ -1,5 +1,34 @@
+import styled from "styled-components";
+import { theme } from "../../../theme";
+
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <LogoStyled>
+      <span>crazee</span>
+      <img src="/assets/images/logo.png" alt="" />
+      <span>burger</span>
+    </LogoStyled>
+  );
 };
 
 export default Logo;
+
+const LogoStyled = styled.div`
+  border: 1px solid red;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  width: fit-content;
+  img {
+    width: 200px;
+  }
+
+  span{
+    font-family: "Amatic sc";
+    font-weight: 700;
+    font-size: 110px;
+    color: ${theme.colors.primary};
+  }
+`;
