d99bb4b74e54b7b33bd2c9bdc3b807aa717f312f
tonym <tony.marques@live.fr>
2024-06-16 13:38:45 +0200

    ✨ F03: Button > Create Button

---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import { theme } from "../../../theme";
+
+type ButtonProps = {
+  Icon: JSX.Element;
+  label: string;
+};
+
+const Button = ({ Icon, label }: ButtonProps) => {
+  return (
+    <ButtonStyled>
+      <span>{label}</span>
+      {Icon && Icon}
+    </ButtonStyled>
+  );
+};
+
+export default Button;
+
+const ButtonStyled = styled.button`
+  width: 100%;
+  padding: 18px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${theme.colors.primary_burger};
+  border: 1px solid transparent;
+  cursor: pointer;
+  border-radius: ${theme.borderRadius.round};
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: ${theme.colors.white};
+    border-color: ${theme.colors.primary};
+  }
+
+  &:hover span,
+  &:hover svg {
+    color: ${theme.colors.primary};
+  }
+
+  span {
+    color: ${theme.colors.white};
+    font-weight: 700;
+    font-family: Arial, Helvetica, sans-serif;
+    transition: color 0.3s;
+  }
+
+  svg {
+    font-size: 15px;
+    color: ${theme.colors.white};
+    transition: color 0.3s;
+  }
+`;
