3b2460f25e932b8bfe37f5517a15ed3ebd4383d5
tonym <tony.marques@live.fr>
2024-06-17 00:19:47 +0200

    ✨ F05: MAIN > Add MenuList and MenuItem

--- src/components/pages/OrderPage/Main.tsx
+++ src/components/pages/OrderPage/Main.tsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 import { theme } from "../../../theme";
+import MenuList from "./MenuList";
 
 const Main = () => {
-  return <MainStyled></MainStyled>;
+  return (
+    <MainStyled>
+      <MenuList />
+    </MainStyled>
+  );
 };
 
 export default Main;
@@ -11,4 +16,5 @@ const MainStyled = styled.div`
   background-color: ${theme.colors.background_white};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   flex: 1;
+  overflow-y: hidden;
 `;
