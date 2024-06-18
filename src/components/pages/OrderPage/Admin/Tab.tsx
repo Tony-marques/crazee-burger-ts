a379bee71500363dc3a015034b3a578fd555cd0a
tonym <tony.marques@live.fr>
2024-06-18 11:27:19 +0200

    💄   F07: TAB > Add style for Tab

--- src/components/pages/OrderPage/Admin/Tab.tsx
+++ src/components/pages/OrderPage/Admin/Tab.tsx
@@ -1,5 +1,35 @@
-const Tab = ({ title }) => {
-  return <div>{title}</div>;
+import styled from "styled-components";
+import { theme } from "../../../../theme";
+
+type TabProps = {
+  Icon: JSX.Element;
+  title?: string;
+};
+
+const Tab = ({ Icon, title }: TabProps) => {
+  return (
+    <TabStyled>
+      {Icon}
+      {title && <p>{title}</p>}
+    </TabStyled>
+  );
 };
 
 export default Tab;
+
+const TabStyled = styled.div`
+  padding: 13px 22px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 13px;
+  border: 1px solid black;
+  border-radius: 5px 5px 0px 0px;
+  background-color: ${theme.colors.white};
+  cursor: pointer;
+  color:${theme.colors.greySemiDark};
+
+  svg{
+    font-size: 16px;
+  }
+`;
--- src/components/pages/OrderPage/Admin/Tabs.tsx
+++ src/components/pages/OrderPage/Admin/Tabs.tsx
@@ -1,12 +1,17 @@
+import { AiOutlinePlus } from "react-icons/ai";
+import { FiChevronDown } from "react-icons/fi";
 import styled from "styled-components";
 import Tab from "./Tab";
+import { MdModeEditOutline } from "react-icons/md";
 
 const Tabs = () => {
-  return <TabsStyled>
-    <Tab title="tab1"/>
-    <Tab title="tab2"/>
-    <Tab title="tab2"/>
-  </TabsStyled>;
+  return (
+    <TabsStyled>
+      <Tab Icon={<FiChevronDown />} />
+      <Tab Icon={<AiOutlinePlus />} title="Ajouter un produit" />
+      <Tab Icon={<MdModeEditOutline />} title="Modifier un produit" />
+    </TabsStyled>
+  );
 };
 
 export default Tabs;
@@ -15,4 +20,6 @@ const TabsStyled = styled.div`
   height: 44px;
   border: 1px solid blue;
   display: flex;
+  gap: 1px;
+  padding-left: 71px;
 `;
