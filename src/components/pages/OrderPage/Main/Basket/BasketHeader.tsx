dde6dd3cdb224246689c9b13ec3b2010dfdba1e1
tonym <tony.marques@live.fr>
2024-06-21 12:20:37 +0200

    💄  F10 > BASKETHEADER > Add style in BasketHeader

--- src/components/pages/OrderPage/Main/Basket/BasketHeader.tsx
+++ src/components/pages/OrderPage/Main/Basket/BasketHeader.tsx
@@ -1,12 +1,35 @@
 import styled from "styled-components";
+import { theme } from "../../../../../theme";
 
 const BasketHeader = () => {
-  return <BasketHeaderStyled>Header</BasketHeaderStyled>;
+  return (
+    <BasketHeaderStyled>
+      <div className="price-wrapper">
+        <span className="total">total</span>
+        <span className="total-price">0.00 €</span>
+      </div>
+    </BasketHeaderStyled>
+  );
 };
 
 export default BasketHeader;
 
 const BasketHeaderStyled = styled.div`
-  background-color: blue;
+  display: flex;
+  background-color: ${theme.colors.background_dark};
   height: 70px;
+  font-family: "Amatic SC", sans-serif;
+  font-size: 36px;
+  font-weight: 400;
+  letter-spacing: 2px;
+  color: ${theme.colors.primary};
+
+  .price-wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    padding: 0 16px;
+  }
 `;
